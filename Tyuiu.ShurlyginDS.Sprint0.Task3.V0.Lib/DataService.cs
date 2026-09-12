da221fad8ce9b0520aab59c42c0c5c8251399140
chelovbtw <chelovgd@gmail.com>
2026-09-12 10:32:16 +0500

    Task3

---
+++ Tyuiu.ShurlyginDS.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Tyuiu.ShurlyginDS.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
